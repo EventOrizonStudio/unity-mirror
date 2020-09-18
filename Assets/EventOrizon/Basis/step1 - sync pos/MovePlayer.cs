7aebf85a10368c39361f3078424ee647aa7665e1
alto971 <gwadaldesign@gmail.com>
2020-09-18 09:21:51 +0200

    add step 2

---
+++ Assets/EventOrizon/Basis/step1 - sync pos/MovePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+ 
+
+ //https://gamedevacademy.org/how-to-create-a-multiplayer-game-in-unity/
+ 
+public class MovePlayer : NetworkBehaviour 
+{
+    [SerializeField]
+    private float speed; //vitesse de déplacement
+ 
+    void FixedUpdate () 
+    {
+        if(this.isLocalPlayer) 
+        {
+            float hMove = Input.GetAxis("Horizontal"); 
+            GetComponent<Rigidbody>().velocity = new Vector3(hMove * speed, 0.0f,0.0f); // uniquement sur axe X
+        }
+    }
+}
---
+++ Assets/EventOrizon/Basis/step2 - sync rot and anim/AnimPrefab.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+//https://assetstore.unity.com/packages/3d/characters/humanoids/fantasy/mini-legion-grunt-pbr-hp-polyart-98187
+public class AnimPrefab : NetworkBehaviour {
+
+    [SerializeField]
+    private float speed = 10; //vitesse de déplacement
+
+    [SerializeField]
+    private Vector3 rotVector = new Vector3 (0, 20, 0); //vitesse de rotation
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private NetworkAnimator netAnim;
+
+    void Update () {
+        if (Input.GetMouseButton (2) && this.isLocalPlayer) {
+            Debug.Log ("Pressed left click: attack");
+            //animator.SetTrigger ("hit"); // not working in sync
+
+            netAnim.SetTrigger ("hit");
+        }
+    }
+
+    void FixedUpdate () {
+        if (this.isLocalPlayer) {
+            float rot = Input.GetAxis ("Horizontal"); // rot
+            float move = Input.GetAxis ("Vertical"); //move
+
+            /*if (move != 0f) {
+                animator.SetTrigger ("run");
+            } else {
+                animator.SetTrigger ("stop");
+            }*/
+
+            animator.SetFloat("move", move);
+
+            GetComponent<Rigidbody> ().velocity = transform.forward * move * speed; //new Vector3(0.0f,0.0f,move * speed); // uniquement sur axe Z
+            this.transform.Rotate (rotVector * rot);
+        }
+    }
+}
